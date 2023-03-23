1e77d76f233cedee1679ab0ba8ecc5650f2d1fcd
Taylor <talachmi@gmail.com>
2023-03-23 11:12:02 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import { Navbar, Brand, CTA } from './components';
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+
+const App = () => {
+  return (
+    <div className='App'>
+        <div className='gradient__bg'>
+          <Navbar />
+          <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+    </div>
+  )
+}
+
+export default App
